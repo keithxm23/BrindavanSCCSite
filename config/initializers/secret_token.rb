2218f886a2e947a9adb0d70ec5cdf304ad2b35ac
Keith Mascarenhas <keithxm23@gmail.com>
2013-08-27 19:58:40 +0000

    first commit, with refinery and twitterbootstrap working togetyher

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brinscc::Application.config.secret_token = 'fb0a822a049fcff3bf69a8d0158524c548647fceddcb1d5f5933fcc14964d934e3604a6453c8fbc7f98a4b845d9d3cfc1f54313e2ab814b9ab1db05bbfb808db'
